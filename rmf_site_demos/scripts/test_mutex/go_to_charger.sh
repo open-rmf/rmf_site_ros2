606e771818b52e20db51b4a2a3501497ef14ee51
Xiyu <xiyu@openrobotics.org>
2026-04-09 13:39:39 +0800

    Merge pull request #5 from open-rmf/xiyu/test_mutex
    New test_mutex site map

---
+++ rmf_site_demos/scripts/test_mutex/go_to_charger.sh
@@ -0,0 +1,5 @@
+ros2 run rmf_demos_tasks dispatch_patrol -p TinyRobot_1_charger --use_sim_time -F TinyRobot -R TinyRobot_1
+ros2 run rmf_demos_tasks dispatch_patrol -p TinyRobot_2_charger --use_sim_time -F TinyRobot -R TinyRobot_2
+ros2 run rmf_demos_tasks dispatch_patrol -p TinyRobot_3_charger --use_sim_time -F TinyRobot -R TinyRobot_3
+ros2 run rmf_demos_tasks dispatch_patrol -p TinyRobot_4_charger --use_sim_time -F TinyRobot -R TinyRobot_4
+ros2 run rmf_demos_tasks dispatch_patrol -p TinyRobot_5_charger --use_sim_time -F TinyRobot -R TinyRobot_5
---
+++ rmf_site_demos/scripts/test_mutex/go_to_start.sh
@@ -0,0 +1,5 @@
+ros2 run rmf_demos_tasks dispatch_patrol -p start_1 --use_sim_time -F TinyRobot -R TinyRobot_1
+ros2 run rmf_demos_tasks dispatch_patrol -p start_2 --use_sim_time -F TinyRobot -R TinyRobot_2
+ros2 run rmf_demos_tasks dispatch_patrol -p start_3 --use_sim_time -F TinyRobot -R TinyRobot_3
+ros2 run rmf_demos_tasks dispatch_patrol -p start_4 --use_sim_time -F TinyRobot -R TinyRobot_4
+ros2 run rmf_demos_tasks dispatch_patrol -p start_5 --use_sim_time -F TinyRobot -R TinyRobot_5
